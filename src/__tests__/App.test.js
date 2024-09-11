46a5dc6f23ce08e0333ae9b1d8d10b0780265ace
Jamal Ahmed <jamal.ahmed@student.moringaschool.com>
2024-09-11 10:42:53 +0300

    complete lab

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,10 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+describe('App Component', () => {
+    it('renders without crashing', () => {
+        render(<App />);
+        const linkElement = screen.getByText(/Your code here/i);
+        expect(linkElement).toBeInTheDocument();
+    });
+});
